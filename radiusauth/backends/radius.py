a9f665b62655ef0da50d6efa7c4dd105f2a20ea6
jmish <jmish1987@gmail.com>
2024-10-02 16:43:05 -0500

    Fixed #23 - Authenticate with Window NPS Radius Server Failed

--- radiusauth/backends/radius.py
+++ radiusauth/backends/radius.py
@@ -146,23 +146,27 @@ class RADIUSBackend(object):
         role_class_prefix = app_class_prefix + "role="
 
         for cl in reply['Class']:
-            cl = cl.decode("utf-8")
-            if cl.lower().find(group_class_prefix) == 0:
-                groups.append(cl[len(group_class_prefix):])
-            elif cl.lower().find(role_class_prefix) == 0:
-                role = cl[len(role_class_prefix):]
-                if role == "staff":
-                    is_staff = True
-                elif role == "superuser":
-                    is_superuser = True
-                # Passing "su-staff" through the Class attribute from the RADIUS server
-                # will set both staff and superuser to true for the user.
-                elif role == "su-staff":
-                    is_staff = True
-                    is_superuser = True
-                else:
-                    logging.warning("RADIUS Attribute Class contains unknown role '%s'. Only roles 'staff', "
-                                    "'su-staff' and 'superuser' are allowed" % cl)
+            try:
+                cl = cl.decode("utf-8")
+                if cl.lower().find(group_class_prefix) == 0:
+                    groups.append(cl[len(group_class_prefix):])
+                elif cl.lower().find(role_class_prefix) == 0:
+                    role = cl[len(role_class_prefix):]
+                    if role == "staff":
+                        is_staff = True
+                    elif role == "superuser":
+                        is_superuser = True
+                    # Passing "su-staff" through the Class attribute from the RADIUS server
+                    # will set both staff and superuser to true for the user.
+                    elif role == "su-staff":
+                        is_staff = True
+                        is_superuser = True
+                    else:
+                        logging.warning("RADIUS Attribute Class contains unknown role '%s'. Only roles 'staff', "
+                                        "'su-staff' and 'superuser' are allowed" % cl)
+            except UnicodeDecodeError:
+                logging.warning("RADIUS Attribute Class contains non-unicode value '%s'. Only unicode values are "
+                                "supported" % cl)
         return groups, is_staff, is_superuser
 
     def _radius_auth(self, server, username, password):
